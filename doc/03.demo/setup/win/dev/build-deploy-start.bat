ea54619bcd419038f766b7729cc8694defb3e09b
jimmyblylee <jimmyblylee@126.com>
2017-10-30 21:42:23 +0800

    demo setup dev

---
+++ doc/03.demo/setup/win/dev/build-deploy-start.bat
@@ -0,0 +1,22 @@
+@echo off
+set BASEDIR=%~sdp0
+call %BASEDIR%\setEnv.bat
+
+title Compiling
+call msg info "[INFO] Compiling" & echo.
+pushd %PROJECT_HOME%\apms
+    call mvn clean package
+popd
+
+
+title Redeploying
+call msg info "[INFO] Redeploying" & echo.
+call msg info "[INFO] removing the old" & echo.
+rmdir /S /Q %TOMCAT_HOME%\webapps\apms >NUL 2>NUL
+del /F /Q %TOMCAT_HOME%\webapps\apms.war >NUL 2>NUL
+call msg info "[INFO] deploying the new" & echo.
+copy /Y %PROJECT_HOME%\apms\target\apms.war %TOMCAT_HOME%\webapps\apms.war
+call msg info "[INFO] done" & echo.
+
+title Tomcat
+call startJrebelDebugTomcat.bat
